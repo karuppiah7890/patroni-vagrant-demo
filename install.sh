ae4270a6cf54a2cdf52d5c581db6564e31b7fc78
Karuppiah Natarajan <karuppiah7890@gmail.com>
2018-12-08 23:18:54 +0530

    Add patroni installation with etcd dependency

--- install.sh
+++ install.sh
@@ -4,9 +4,10 @@ sudo add-apt-repository ppa:vbernat/haproxy-1.8 -y
 wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
 sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt/ `lsb_release -cs`-pgdg main" >> /etc/apt/sources.list.d/pgdg.list'
 sudo apt update
-sudo apt install -y postgresql-11 haproxy=1.8.\*
+sudo apt install -y postgresql-11 haproxy=1.8.\* python-pip
 sudo service postgresql stop
 wget https://github.com/etcd-io/etcd/releases/download/v3.3.10/etcd-v3.3.10-linux-amd64.tar.gz
 cd ~ && tar -xvzf etcd-v3.3.10-linux-amd64.tar.gz
 echo "export PATH=$PATH:/usr/lib/postgresql/11/bin:~/etcd-v3.3.10-linux-amd64/" > ~/.bashrc
 source ~/.bashrc
+pip install patroni[etcd]
